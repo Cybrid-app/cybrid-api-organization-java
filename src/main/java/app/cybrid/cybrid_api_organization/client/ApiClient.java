724ca04db23d8f0828e46f4bcb9b71a89d907393
brent-cybrid <brent@cybrid.app>
2023-08-21 19:22:35 +0000

    v0.85.5

--- src/main/java/app/cybrid/cybrid_api_organization/client/ApiClient.java
+++ src/main/java/app/cybrid/cybrid_api_organization/client/ApiClient.java
@@ -69,7 +69,7 @@ import app.cybrid.cybrid_api_organization.client.auth.HttpBearerAuth;
 import app.cybrid.cybrid_api_organization.client.auth.ApiKeyAuth;
 import app.cybrid.cybrid_api_organization.client.auth.OAuth;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-08-19T21:41:27.269012Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-08-21T19:18:35.813685Z[Etc/UTC]")
 public class ApiClient extends JavaTimeFormatter {
     public enum CollectionFormat {
         CSV(","), TSV("\t"), SSV(" "), PIPES("|"), MULTI(null);
--- src/main/java/app/cybrid/cybrid_api_organization/client/auth/HttpBasicAuth.java
+++ src/main/java/app/cybrid/cybrid_api_organization/client/auth/HttpBasicAuth.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.util.Base64Utils;
 import org.springframework.util.MultiValueMap;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-08-19T21:41:27.269012Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-08-21T19:18:35.813685Z[Etc/UTC]")
 public class HttpBasicAuth implements Authentication {
     private String username;
     private String password;
