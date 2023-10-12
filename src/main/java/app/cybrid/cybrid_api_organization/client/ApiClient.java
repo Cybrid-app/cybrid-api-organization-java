310a58dd316b495e32117efe3180158164f574b2
brent-cybrid <brent@cybrid.app>
2023-10-12 21:54:49 +0000

    v0.106.1

--- src/main/java/app/cybrid/cybrid_api_organization/client/ApiClient.java
+++ src/main/java/app/cybrid/cybrid_api_organization/client/ApiClient.java
@@ -69,7 +69,7 @@ import app.cybrid.cybrid_api_organization.client.auth.HttpBearerAuth;
 import app.cybrid.cybrid_api_organization.client.auth.ApiKeyAuth;
 import app.cybrid.cybrid_api_organization.client.auth.OAuth;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-10-12T18:35:45.101867Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2023-10-12T21:49:46.104252Z[Etc/UTC]")
 public class ApiClient extends JavaTimeFormatter {
     public enum CollectionFormat {
         CSV(","), TSV("\t"), SSV(" "), PIPES("|"), MULTI(null);
