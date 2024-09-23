cc47dd9d944a7a83063b545188bb3ba3f23cecd1
brent-cybrid <brent@cybrid.app>
2024-09-23 23:36:08 +0000

    v0.119.134

--- src/main/java/app/cybrid/cybrid_api_organization/client/auth/OAuth.java
+++ src/main/java/app/cybrid/cybrid_api_organization/client/auth/OAuth.java
@@ -3,7 +3,7 @@ package app.cybrid.cybrid_api_organization.client.auth;
 import org.springframework.http.HttpHeaders;
 import org.springframework.util.MultiValueMap;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-09-23T20:25:05.751154Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-09-23T23:30:19.173745Z[Etc/UTC]")
 public class OAuth implements Authentication {
     private String accessToken;
 
