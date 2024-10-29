e586974bf9f2e8dd0583ee84fb8becfdef260575
brent-cybrid <brent@cybrid.app>
2024-10-29 23:17:43 +0000

    v0.121.55

--- src/main/java/app/cybrid/cybrid_api_organization/client/api/SubscriptionDeliveriesOrganizationApi.java
+++ src/main/java/app/cybrid/cybrid_api_organization/client/api/SubscriptionDeliveriesOrganizationApi.java
@@ -28,7 +28,7 @@ import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.Flux;
 
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-10-29T04:52:40.922549Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2024-10-29T23:13:30.799364Z[Etc/UTC]")
 public class SubscriptionDeliveriesOrganizationApi {
     private ApiClient apiClient;
 
